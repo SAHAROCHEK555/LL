2d07a7131309e8ffd2790e8145450da2b5be7b76
Aleksey Loktionov <sams12.34.58@gmail.com>
2024-11-22 03:14:51 +0500

    add flashcard in modules_kb

--- app/keyboards/modules_select_kb.py
+++ app/keyboards/modules_select_kb.py
@@ -5,6 +5,7 @@ modules_inline = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Переводчик", callback_data="translate")],
     [InlineKeyboardButton(text="Упражнения", callback_data="exercises")],
     [InlineKeyboardButton(text="Объяснения", callback_data="explation")],
+    [InlineKeyboardButton(text="Карточки для запоминания", callback_data="flashcards")],
     [InlineKeyboardButton(text="Назад⬛️", callback_data="back_to_level_select")]
 ])
 
