31903489f0953e30861435d771b2fcf77f9ce990
Klikmok <ribakov2007@icloud.com>
2024-11-22 06:53:35 +0500

    remove junk functions, add gramar and translate exercises

--- app/keyboards/translate_keyboard.py
+++ app/keyboards/translate_keyboard.py
@@ -2,5 +2,5 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 back_to_input_words = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Назад⬛️", callback_data="back_to_input_words no")]
+    [InlineKeyboardButton(text="Назад⬛️", callback_data="translate")]
 ])
